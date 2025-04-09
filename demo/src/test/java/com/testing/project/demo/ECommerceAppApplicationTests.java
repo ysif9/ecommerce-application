6448214e998d176bfee86bd582d70d73c7b390df
Saifeldinsais <saifeldinsais@gmail.com>
2025-04-10 00:32:32 +0200

    ecommerce

---
+++ demo/src/test/java/com/testing/project/demo/ECommerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testing.project.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
