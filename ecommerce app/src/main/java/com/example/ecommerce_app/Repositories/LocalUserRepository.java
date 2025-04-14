f776967cf1d7170a111a47e5f293770c6d7ddd74
Yousif Abdulhafiz <89231113+ysif9@users.noreply.github.com>
2025-04-14 23:43:43 +0200

    Delete ecommerce app/src/main/java/com/example/ecommerce_app/Repositories/LocalUserRepository.java

--- ecommerce app/src/main/java/com/example/ecommerce_app/Repositories/LocalUserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ecommerce_app.Repositories;
-
-import com.example.ecommerce_app.Model.LocalUser;
-import org.springframework.data.repository.ListCrudRepository;
-
-public interface LocalUserRepository extends ListCrudRepository<LocalUser, Long> {
-}
